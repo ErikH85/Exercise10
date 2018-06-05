a8ac663741e3f89bb15c8074ebb275d12abb13cb
Erik <erikhansson85@gmail.com>
2018-06-05 10:54:09 +0200

    refactoring

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -5,11 +5,11 @@ public class BubbleSort {
     public int[] sortArray(int[] numbers){
 
         for (int i = 0; i < numbers.length ; i++) {
-            for (int j = 1; j < numbers.length ; j++) {
-                if(numbers[i] < numbers[j]){
+            for (int j = 1; j < numbers.length  ; j++) {
+                if(numbers[i] < numbers[j-1]){
                     int temp = numbers[i];
-                    numbers[i] = numbers[j];
-                    numbers[j] = temp;
+                    numbers[i] = numbers[j-1];
+                    numbers[j-1] = temp;
                 }
             }
         }

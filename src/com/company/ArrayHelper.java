b991a23e3fbb3149c8d869186d8d9b79438f00e9
Erik <erikhansson85@gmail.com>
2018-06-05 09:27:59 +0200

    övning 1 klar

--- src/com/company/ArrayHelper.java
+++ src/com/company/ArrayHelper.java
@@ -8,4 +8,18 @@ public class ArrayHelper {
         int max = Arrays.stream(a).max().getAsInt();
         return max;
     }
+
+    public int minNumber(int[] a){
+        int min = Arrays.stream(a).min().getAsInt();
+        return min;
+    }
+
+    public int sum(int[] a){
+        int sum = 0;
+        for (int s: a) {
+            sum = sum + s;
+        }
+
+        return sum;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,7 +8,9 @@ public class Main {
 
         int[] intArr =  {10,20,30,40,50};
         ArrayHelper ah = new ArrayHelper();
-        System.out.println(ah.maxNumber(intArr));
+        System.out.println("största värde: " + ah.maxNumber(intArr));
+        System.out.println("minsta värde: " + ah.minNumber(intArr));
+        System.out.println("summan: " + ah.sum(intArr));
 
     }
 

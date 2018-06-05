ad095255adcbcda2d75ad5edbe7d0db08009bac6
Erik <erikhansson85@gmail.com>
2018-06-05 11:24:14 +0200

    refactor

--- src/com/company/LottoGenerator.java
+++ src/com/company/LottoGenerator.java
@@ -6,17 +6,17 @@ public class LottoGenerator {
 
     public void generateNumbers(){
         Random rand = new Random();
-        int[] numbers = new int[8];
+        int[] numbers = new int[7];
+
+        numbers[0] = rand.nextInt(35)+1;
         numbers[1] = rand.nextInt(35)+1;
         numbers[2] = rand.nextInt(35)+1;
         numbers[3] = rand.nextInt(35)+1;
         numbers[4] = rand.nextInt(35)+1;
         numbers[5] = rand.nextInt(35)+1;
         numbers[6] = rand.nextInt(35)+1;
-        numbers[7] = rand.nextInt(35)+1;
 
-        for (int i = 0; i < numbers.length ; i++) {
-
+        for (int i = 0; i < numbers.length; i++) {
             for (int j = 0; j < numbers.length; j++) {
                 if(numbers[i] == numbers[j]){
                     numbers[i] = rand.nextInt(35)+1;
@@ -25,7 +25,7 @@ public class LottoGenerator {
         }
 
         System.out.println
-                ("Lottorad: " + numbers[1] + " " + numbers[2] + " " + numbers[3] + " " +  numbers[4]
-                        + " " + numbers[5] + " " + numbers[6] + " " + numbers[7]);
+                ("Lottorad: " + numbers[0] + " " + numbers[1] + " " + numbers[2] + " " +  numbers[3]
+                        + " " + numbers[4] + " " + numbers[5] + " " + numbers[6]);
     }
 }
